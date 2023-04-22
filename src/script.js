fd6a7b07edf61718773680321fe7f18f9693c9bb
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-04-22 21:52:00 +0200

    added social-links

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 const boxes = document.querySelectorAll(".article-me");
+
 let checkBoxes = () => {
-  const triggerBottom = window.innerHeight / 5 * 4;
-  boxes.forEach((box) =>{
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top;
 
-    if(boxTop < triggerBottom){
-        box.classList.add("show");
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
     }
     // else{
     //     box.classList.remove("show");
